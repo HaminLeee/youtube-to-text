ee27f75d9a2e57f57dde662abeb0352c10efd7d1
haminthecoder <haminn.lee@mail.utoronto.ca>
2022-01-18 22:11:05 -0500

    add styling, comments, readme demo

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# speech2text-youtube
+# Youtube To Text
 Given a youtube link, this code will extract the audio and run Speech2Text, outputting the transcript.
+# Speech-to-Text
+
+This repository makes use of [Speech-to-text](https://github.com/snakers4/silero-models), to extract the transcript from a youtube video. 
+
+
+## Installation
+```
+pip install -r requirements.txt
+```
+## Demo
+### Using Web Cam
+<img src="./output.gif" style="width:30rem;"/>
+<!-- ![Alt Text](./output.gif) -->
+
+
+## Getting Started
+```
+python3 app.py
+```
+
+
+## Tools Used
+[PyTorch Hub](https://pytorch.org/hub/)
+
+[Pafy](https://pypi.org/project/pafy/)
+
+[FFMPEG](https://www.ffmpeg.org/)
--- app.py
+++ app.py
@@ -1,52 +1,59 @@
 import torch
-# import torchaudio
 from glob import glob
 from flask import Flask,render_template,request
 import pafy
 import os
 import re
 
-
+# To check if its a valid youtube link
 YOUTUBE_REGEX="^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
+# Create flask app
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=[ "GET", "POST" ])
-def hello_world():
-    # print(speech2text())
-    # transcript = speech2text("./audios/out/out.wav")
-    transcript, youtube_response, youtube_link, loading = "", "", "", ""
+def run():
+    # Initialize template params
+    transcript, youtube_link = "", ""
+
+    # If user submits the youtube link
     if request.method == "POST":
         youtube_link = request.form.get("youtube_link")
-        youtube_regex = YOUTUBE_REGEX
-        loading = "loading..."
+
+        # Parse the youtube link
         video = youtube_parser(youtube_link)
-        youtube_response = 'Title: {}, UserName: {}, Views: {}, Likes: {}'.format(video.title, video.username, video.viewcount, video.likes) if re.match(youtube_regex, youtube_link) != None else 'Invalid Link'
+
+        # Create transcript using speech-to-text
         transcript = speech2text("./audios/out/out.wav")
 
-        loading = ""
-        return render_template("index.html", transcript=transcript, youtube_link=youtube_link, thumbnail=video.thumb, youtube_response=youtube_response, loading=loading)
+        return render_template("index.html", transcript=transcript, youtube_link=youtube_link, video=video)
 
+    # When user haven't submitted yet
     if request.method == "GET":
+        # Create transcript if output file exists
         transcript = speech2text("./audios/out/out.wav")
-        return render_template("index.html", transcript=transcript, youtube_link=youtube_link, youtube_response=youtube_response)
+        return render_template("index.html", transcript=transcript, youtube_link=youtube_link)
 
-def youtube_parser(url):    
+def youtube_parser(url):   
+    # Remove in.m4a if it exists 
     os.system("rm -f ./audios/in/in.m4a")
-    # instant created
+
+    # Parse video using pafy
     video = pafy.new(url)
     
+    # Get audio stream and download it
     audiostreams = video.audiostreams
-    print(audiostreams)
-    
     bestaudio = video.getbestaudio()
     bestaudio.download("./audios/in/in.m4a")
 
+    # Convert m4a file to wav file using ffmpeg
     os.system("ffmpeg -i ./audios/in/in.m4a ./audios/out/out.wav -y")
     os.system("rm -f ./audios/in/in.m4a")
 
     return video
 
 
+# https://pytorch.org/hub/snakers4_silero-models_stt/
 def speech2text(input_file):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # gpu also works, but our models are fast enough for CPU
 
@@ -61,13 +68,16 @@ def speech2text(input_file):
     batches = split_into_batches(test_files, batch_size=10)
     input = prepare_model_input(read_batch(batches[0]),
                                 device=device)
-    print("===== Speech2Text Started ... =====")
+                                
+    print("\n\n===== Speech2Text Started ... =====\n\n")
+
     output = model(input)
     res = ""
     for example in output:
         res += decoder(example.cpu())
-    print("\n\noutput: " + res + "\n\n")
-    print("Aborting.. \n\n===== Speech2Text Done =====")
+
+    print("Output: \n\n" + res + "\n\n")
+    print("Aborting.. \n\n===== Speech2Text Done =====\n\n")
 
     return res
 
